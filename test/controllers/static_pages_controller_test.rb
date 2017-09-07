7ce0e239fc9f8ebfecc99c1ef93d65abc6abac80
Makoto Otsuka <mametra2580@gmail.com>
2017-09-07 11:37:17 +0900

    titleを変更しました　

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,23 +2,36 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get home" do
+  def setup
+    @base_title = "MiyazakiPark"
+  end
+
+  test "should get root" do
     get root_path
     assert_response :success
   end
 
-  test "should get help" do
-    get helf_path
+  test "should get home" do
+    get home_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
