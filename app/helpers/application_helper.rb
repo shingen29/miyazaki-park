6e239dbabf94587a713002b6de0d5e91a4dc1c08
Makoto Otsuka <mametra2580@gmail.com>
2017-09-07 20:16:39 +0900

    ページごとのタイトルを返すヘルパーを追加した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとの完全案タイトルを返します。
+  def full_title(page_title = '')
+    base_title = "MiyazakiPark"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
