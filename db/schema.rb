48f1dfcf174adfd8cfce27bd1870b97190241dfc
Makoto Otsuka <mametra2580@gmail.com>
2017-10-19 19:48:39 +0900

    Usersモデルにpassword_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012112916) do
+ActiveRecord::Schema.define(version: 20171019104646) do
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20171012112916) do
     t.integer "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
